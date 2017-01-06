68d3cab19e3922881ec7af676712bc338747b5ca
Tamas Verhoest <Tamas@tamass-mac.local>
2017-01-06 14:45:06 +0100

    Fix

--- SportsBattle/LocationViewController.swift
+++ SportsBattle/LocationViewController.swift
@@ -4,7 +4,7 @@ import Foundation
 
 class LocationViewController: UITableViewController {
     
-    fileprivate var sportLocations: [SportLocation] = []
+    var sportLocations: [SportLocation] = []
     
     private var currentTask: URLSessionTask?
     
@@ -13,83 +13,23 @@ class LocationViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return sportLocations.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "locationCell", for: indexPath)
        
-        //let location = sportLocations[indexPath.row]
-        //cell.textLabel!.text = location.name
-        //cell.detailTextLabel!.text = location.wijk
+        let location = sportLocations[indexPath.row]
+        cell.textLabel!.text = location.name
+        cell.detailTextLabel!.text = location.wijk
         return cell
         
     }
    
     override func viewDidLoad() {
       
-        getHttpData(urlAddress: "https://datatank.stad.gent/4/cultuursportvrijetijd/buurtsportlocaties.json")
+        
     }
     
-    func getHttpData(urlAddress : String)
-    {
-        // Asynchronous Http call to your api url, using NSURLSession:
-        guard let url = URL(string: urlAddress) else
-        {
-            print("Url conversion issue.")
-            return
-        }
-        URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) -> Void in
-            // Check if data was received successfully
-            if error == nil && data != nil {
-                do {
-                    // Convert NSData to Dictionary where keys are of type String, and values are of any type
-                    let json = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String:AnyObject]
-                    
-                    //print(json)
-                    //let str = json["totalFeatures"] as! Int
-                    let features = json["features"] as! [AnyObject]
-                    
-                    var location: SportLocation
-                    
-                    for index in 0...features.count - 1 {
-                        let feature = features[index] as! [String:AnyObject]
-                        
-                        let properties = feature["properties"] as! [String:AnyObject]
-                        let geometry = feature["geometry"] as! [String:AnyObject]
-                        
-                        let coordinates = geometry["coordinates"] as! [Double]
-                        let wijk = properties["Wijk"] as! String
-                        
-                        let name = properties["Naam"] as! String
-                        let lat = coordinates[0]
-                        let long = coordinates[1]
-                        
-                        let coo = CLLocationCoordinate2D(latitude: lat, longitude: long)
-                        
-                        location = SportLocation(name: name, location: coo, wijk: wijk)
-                        
-                        self.sportLocations.append(location)
-                        
-                    }
-                    
-                     
-                    
-                    print(self.sportLocations.count)
-                    
-                } catch {
-                    print(error)
-                    // Something went wrong
-                }
-            }
-            else if error != nil
-            {
-                print(error)
-            }
-        }).resume()
-    }
-    
-     
-
 }
